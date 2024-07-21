167f53d94aa92f39757ef35baed23b2cdf4f2cca
Kiran Kulkarni <34923686+KiranVK98@users.noreply.github.com>
2024-07-21 01:38:05 -0500

    Update IsSubSequence.py

--- GitTop150/Git_TwoPointer/IsSubSequence.py
+++ GitTop150/Git_TwoPointer/IsSubSequence.py
@@ -1,20 +1,36 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        # letterDistinct = set()
-        last_found = 0
-        found = 0
-        if(len(s) == 0):
-            return True
-        for key,letter in enumerate(s):
+        i, j = 0, 0
+        #Two pointer Solutiion
+        while(i < len(s) and j < len(t)):
+            if(s[i] == t[j]):
+                i += 1
+                j += 1
 
-            for l in range(last_found,len(t)):
-                if(letter == t[l]):
-                    last_found = l+1
-                    found += 1
-                    # print(l,t[l])
-                    if(found == len(s)):
-                        return True
-                    break
+            else:
+                j += 1
+
+    
+        return True if i == len(s) else False
+
+
+
+
+        # letterDistinct = set()
+        # last_found = 0
+        # found = 0
+        # if(len(s) == 0):
+        #     return True
+        # for key,letter in enumerate(s):
+
+        #     for l in range(last_found,len(t)):
+        #         if(letter == t[l]):
+        #             last_found = l+1
+        #             found += 1
+        #             # print(l,t[l])
+        #             if(found == len(s)):
+        #                 return True
+        #             break
                 
                 
 
