b76259b62e073c533b9dd823a4bea324255d060d
Kiran Kulkarni <34923686+KiranVK98@users.noreply.github.com>
2024-08-11 15:26:17 -0500

    Update 904_FruitIntoBasket.py

--- GitTop150/SlidingWindow/904_FruitIntoBasket.py
+++ GitTop150/SlidingWindow/904_FruitIntoBasket.py
@@ -1,55 +1,87 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        hash_map = {}
+        hash_map = collections.defaultdict(int)
 
         #in a subarray there has to be at most two different fruits
         #we need maximum fruits that we an collect within this sub array
 
         left = 0
 
+        total_fruits = 0
 
         max_fruits = 0
 
         for right in range(len(fruits)):
-            if(len(hash_map) <= 2):
-                if(fruits[right] not in hash_map):
-                    hash_map[fruits[right]] = 1
-                    # print(right)
-                else:
-                    hash_map[fruits[right]] += 1
+            hash_map[fruits[right]] += 1
+            total_fruits += 1
 
-                # print(left,right,hash_map,len(hash_map))
-                if(len(hash_map) <= 2):
-                    max_fruits = max(max_fruits, right - left+1)
-                else:
-                    max_fruits = max(max_fruits, right - left)
+            while(len(hash_map) > 2):
+                hash_map[fruits[left]] -= 1
+                if(hash_map[fruits[left]] == 0):
+                    hash_map.pop(fruits[left])
+                total_fruits -= 1
+                left += 1
 
+            max_fruits = max(total_fruits, max_fruits)
 
 
+        return max_fruits
 
-            else:
-                if(fruits[right] not in hash_map):
-                    hash_map[fruits[right]] = 1
-                    # print(right)
-                else:
-                    hash_map[fruits[right]] += 1
-                while(left < right and len(hash_map) > 2):
-                    if(hash_map[fruits[left]] == 1):
-                        del[hash_map[fruits[left]]]
-                    else:
-                        hash_map[fruits[left]] -= 1
 
-                    # max_fruits = max(max_fruits, right - left)
-                    left += 1
-                    # print(hash_map)
 
-                # print(left, right)
-                max_fruits = max(max_fruits, right - left+1)
+
+# class Solution:
+#     def totalFruit(self, fruits: List[int]) -> int:
+#         hash_map = {}
+
+#         #in a subarray there has to be at most two different fruits
+#         #we need maximum fruits that we an collect within this sub array
+
+#         left = 0
+
+
+#         max_fruits = 0
+
+#         for right in range(len(fruits)):
+#             if(len(hash_map) <= 2):
+#                 if(fruits[right] not in hash_map):
+#                     hash_map[fruits[right]] = 1
+#                     # print(right)
+#                 else:
+#                     hash_map[fruits[right]] += 1
+
+#                 # print(left,right,hash_map,len(hash_map))
+#                 if(len(hash_map) <= 2):
+#                     max_fruits = max(max_fruits, right - left+1)
+#                 else:
+#                     max_fruits = max(max_fruits, right - left)
+
+
+
+
+#             else:
+#                 if(fruits[right] not in hash_map):
+#                     hash_map[fruits[right]] = 1
+#                     # print(right)
+#                 else:
+#                     hash_map[fruits[right]] += 1
+#                 while(left < right and len(hash_map) > 2):
+#                     if(hash_map[fruits[left]] == 1):
+#                         del[hash_map[fruits[left]]]
+#                     else:
+#                         hash_map[fruits[left]] -= 1
+
+#                     # max_fruits = max(max_fruits, right - left)
+#                     left += 1
+#                     # print(hash_map)
+
+#                 # print(left, right)
+#                 max_fruits = max(max_fruits, right - left+1)
                 
 
 
 
 
-        return max_fruits
+#         return max_fruits
 
             
