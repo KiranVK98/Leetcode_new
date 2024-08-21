ce4fd29cbe8e33fc6bdf0c9312a4390c0d2a5ba4
Kiran Kulkarni <34923686+KiranVK98@users.noreply.github.com>
2024-08-21 18:55:29 -0500

    Create 56_MergeIntervals.py

---
+++ GitTop150/Intervals/56_MergeIntervals.py
@@ -0,0 +1,18 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+        intervals.sort()
+        for interval in intervals:
+            if not res:
+                res.append(interval)
+
+            else:
+
+                if(interval[0] <= res[-1][1]):
+                    res[-1][1] = max(interval[1], res[-1][1])
+
+                else:
+                    res.append(interval)
+
+
+        return res
